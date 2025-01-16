8a28da5c94c179ec0db08c454d0e385e807d476f
Meekies <viso018@student.kristiania.no>
2025-01-16 11:54:18 +0100

    Endret tekst

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,4 @@ import { createRoot } from "react-dom/client";
 import React from "react";
 import App from "./App";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("viviana")).render(<h2>Hello peepz</h2>);
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/kws2100-lecture2",
+  base: "/IntelliJ",
 });
